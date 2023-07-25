912851b19f608a2dea2514494a627fd3516c3626
methertb <anuragyadavmnp@gmail.com>
2023-07-25 16:28:03 +0530

    website updates fully

---
+++ include/footer.php
@@ -0,0 +1,67 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/restatelg_white.png" alt="image"></a> </div>
+                                        <p class="pb-20 text-white">This website is a platform for connecting landlords and tenants for PG rentals. We do not guarantee the accuracy or availability of the listed properties. It is the responsibility of users to verify the information and make informed decisions.</p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Support</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-white">Forum</a></li>
+                                                    <li><a href="#" class="text-white">Terms and Condition</a></li>
+                                                    <li><a href="#" class="text-white">Frequently Asked Question</a></li>
+                                                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Quick Links</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">About Us</a></li>
+                                                    <li><a href="#" class="text-white">Featured PGs</a></li>
+                                                    <li><a href="#" class="text-white">Submit Property</a></li>
+                                                    <li><a href="agent.php" class="text-white">Our Agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">Contact Us</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>Modern Valley, Kharar</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+91 981-553-3309</li>
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+91 809-176-8971</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>info@pgconnect.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="#"><i class="fas fa-rss"></i></a> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6"> <span class="text-white">© <?php echo date('Y');?> PG Connect - Developed By Anurag yadav</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-white hover-text-primary float-right">
+                            <li><a href="#">Privacy & Policy</a></li>
+                            <li>|</li>
+                            <li><a href="#"> Site Map</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
